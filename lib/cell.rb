671165759251a0658522e1af6dd11232a9b7835e
Candice Cirbo <thecirbos@icloud.com>
2024-05-24 17:29:03 -0600

    added methods and tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,19 +1,50 @@
 class Cell
 
     attr_reader :coordinate,
-                :ship
+                :ship,
+                :fired_upon
 
     def initialize(coordinate)
         @coordinate = coordinate
         @ship = nil
+        @fired_upon = false
     end
 
-   
     def empty?
-     @ship == nil
+        @ship == nil
     end
 
     def place_ship(cruiser)
         @ship = cruiser
+    end 
+
+    def fired_upon?
+        @fired_upon
     end
+
+    def fire_upon
+        @fired_upon = true
+        if !empty? 
+            @ship.hit
+        end           
+    end
+
+    def render(show_ship = false)
+        if show_ship && !empty?
+            "S"
+        elsif @fired_upon == false
+            "."  
+        elsif @fired_upon && empty? 
+            "M"   
+
+        elsif @fired_upon && !empty? && @ship.sunk?
+            "X"
+
+        elsif @fired_upon && !empty? 
+            "H"               
+       
+        end
+    end
+
 end
+
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,7 +4,6 @@ require './lib/ship'
 require './lib/cell'
 
 
-
 RSpec.configure do |config|
     config.formatter = :documentation
   end
