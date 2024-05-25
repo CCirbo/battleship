8e42a5613d0499220e47e9bc8470141e0d06c91f
Candice Cirbo <thecirbos@icloud.com>
2024-05-25 13:33:18 -0600

    added more tests and methods

--- lib/board.rb
+++ lib/board.rb
@@ -17,8 +17,82 @@ class Board
     hash 
    
   end
+
   def valid_coordinate?(coordinate)
     @cells.has_key?(coordinate)
   end
- 
+
+  def valid_placement?(ship, coordinates)
+# @cells, ship and coordinate
+    # @cells is a hash 
+        # the keys are the coordinates such as "A1"
+        # the values are the instance of the cell
+        # class
+    # ship is an instance of the ship class
+        # attrs :  name, length, health,
+        # methods: hit and sunk?
+    # coordinate is an array that contains strings
+        # ["A1", "A3"]
+
+    # We want to make sure when we are placing our 
+        # ships on the board that there is actual
+        # valid space available.
+    # Making sure ship length equals the coordinate length.
+    
+    # if ship coordinates are next to each other
+        # than true, if not, it's false
+    ship.length == coordinates.length
+  end
+  
+  def are_coordinates_consecutive?(coordinates)
+
+    consecutive --> horizontally or vertically
+        - horizontally
+            - does the first letter match
+            - do the numbers increase by 1
+        - vertically
+                
+
+#How do find if the coord are next to each other?
+        # horizontally and vertically
+    # For horizontally consecutive numbers to one
+        # letter
+    # is the ship
+
+    # ship - attrs: name, length, health; methods: hit and sunk?
+    # coordinate - ["A1", "A2", "A4"]
+
+    # start with coordinate variable passed in
+    # horizontally consecutive
+        # make sure letter is the same
+            # an array of two-char strings, first char is the same letter
+                # need to iterate over the array and look at the first char
+                #which letter and make sure it matches
+                    # get the first char from the first element in array
+                        # assign it variable char_to_verify
+                   
+                    char_to_verify = coordinates[0][0]
+                    verified = nil
+                    # iterate over array
+                        # access first char in the string
+                            # assign it to var to string_char
+                        # check if string_char is the same as char_to_verify
+                    coordinates.each do |coordinate|
+                        # "a1"
+                        string_char = coordinate[0]
+                        if string_char == char_to_verify
+                         # keep going
+                            verified = true
+                        else
+                            # stop or end or fail the check
+                            verified = false
+                            break
+                        end
+                    end
+                    return verified
+  end 
+                
+                # make sure numbers increase by 1
+                    # array of 2-char strings, sec char is digit increasing by one
+           
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,6 +10,8 @@ RSpec.configure do |config|
   RSpec.describe Board do
     before(:each) do
         @board = Board.new
+        @cruiser = Ship.new("Cruiser", 3)
+        @submarine = Ship.new("Submarine", 2)
     end
 
   it 'can exist' do
@@ -20,12 +22,29 @@ RSpec.configure do |config|
     expect(@board.cells["D4"]).to be_an_instance_of(Cell)
   end
 
-  it 'can validate coordiante' do
+  it 'can validate coordinate' do
     expect(@board.valid_coordinate?("A1")).to be true
     expect(@board.valid_coordinate?("D4")).to be true
     expect(@board.valid_coordinate?("A5")).to be false
     expect(@board.valid_coordinate?("E1")).to be false
     expect(@board.valid_coordinate?("A22")).to be false
   end
+
+  it 'can check if there is a valid placement of ship object' do
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  it 'can check if the first character of '
+
+  it 'can check if the coordinates are consecutive' do
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(@board.valid_placement?(@submarine ["A1", "C1"])).to eq(false)
+    expect(@board.valid_placement?(@cruiser ["A3", "A2", "A1"])).to eq(false)
+    expect(@board.valid_placement?(@submarine ["C1", "B1"])).to eq(false)
+    
+  end
+  
+
 
 end
