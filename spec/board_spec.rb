06304e005ec34e8e9497b40a49793fcc7027278a
Candice Cirbo <thecirbos@icloud.com>
2024-05-25 19:16:15 -0600

    added tests for valid_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -30,18 +30,25 @@ RSpec.configure do |config|
     expect(@board.valid_coordinate?("A22")).to be false
   end
 
-  it 'can check if there is a valid placement of ship object' do
+   it 'can check if there is a valid placement of ship object' do
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "D7"])).to eq(false)
+
+    # this one tests horizontal placement
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to eq(true)
+
+    # this one tests vertical placement
+    expect(@board.valid_placement?(@cruiser, ["A1", "B1", "C1"])).to eq(true)
+
     expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
     expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to eq(false)
   end
 
-  it 'can check if the first character of '
-
-  it 'can check if the coordinates are consecutive' do
+ 
+    it 'can check if the coordinates are consecutive' do
     expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq(false)
-    expect(@board.valid_placement?(@submarine ["A1", "C1"])).to eq(false)
-    expect(@board.valid_placement?(@cruiser ["A3", "A2", "A1"])).to eq(false)
-    expect(@board.valid_placement?(@submarine ["C1", "B1"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq(false)
+    expect(@board.valid_placement?(@cruiser,["A3", "A2", "A1"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to eq(false)
     
   end
   
