69dda8c87df47c0f8aee9dc280bca343377ec0c6
Candice Cirbo <thecirbos@icloud.com>
2024-05-27 15:42:24 -0600

    added tests for render of board and minor spacing changes

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -31,9 +31,9 @@ RSpec.configure do |config|
   end
 
    it 'can check if the number of coordinates in the array are the same as the length of the ship' do
-        expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
-        expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to eq(false)
-        expect(@board.valid_placement?(@submarine, ["A2", "A3"])).to eq(true)
+       expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
+       expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to eq(false)
+       expect(@board.valid_placement?(@submarine, ["A2", "A3"])).to eq(true)
     end
 
     it 'can check if there is a valid horizontal placement' do
@@ -46,8 +46,8 @@ RSpec.configure do |config|
 
     it 'can check if there is a valid vertical placement' do
     # this one tests vertical placement
-        expect(@board.valid_placement?(@cruiser, ["A1", "B1", "C1"])).to eq(true)
-        expect(@board.valid_placement?(@cruiser, ["A3", "B2", "C3"])).to eq(false)
+       expect(@board.valid_placement?(@cruiser, ["A1", "B1", "C1"])).to eq(true)
+       expect(@board.valid_placement?(@cruiser, ["A3", "B2", "C3"])).to eq(false)
 
     end
     
@@ -80,5 +80,65 @@ RSpec.configure do |config|
         expect(@board.valid_placement?(@submarine, ["A1", "B1"])).to be false
     end
 
+  describe '#render' do  
+    it 'can render the board as a string representation of itself' do
+      @board.place_ship(@cruiser, ["A1", "A2", "A3"])
+      expect(@board.render).to eq( "  1 2 3 4 \n" +
+                                   "A . . . . \n" +
+                                   "B . . . . \n" +
+                                   "C . . . . \n" +
+                                   "D . . . . \n")
+    end
+      it 'can render the board so that it shows ships' do
+      @board.place_ship(@cruiser, ["A1", "A2", "A3"])
+      # expect(@board.cells["A1"].ship).to eq(@cruiser)
+      # expect(@board.cells["A2"].ship).to eq(@cruiser)
+      # expect(@board.cells["A3"].ship).to eq(@cruiser)
+      expect(@board.render(true)).to eq("  1 2 3 4 \n" +
+                                        "A S S S . \n" +
+                                        "B . . . . \n" +
+                                        "C . . . . \n" +
+                                        "D . . . . \n")                 
+      end 
+
+     it 'can show if board has misses and hits' do
+      @board.place_ship(@cruiser, ["A1", "A2", "A3"])
+      @board.cells["A1"].fire_upon
+      @board.cells["B4"].fire_upon
+      expect(@board.render).to eq("  1 2 3 4 \n" +
+                                  "A H . . . \n" +
+                                  "B . . . M \n" +
+                                  "C . . . . \n" +
+                                  "D . . . . \n")
+      end 
+
+      it 'can show sunk ships' do
+      @board.place_ship(@submarine, ["C1", "D1"])
+      @board.cells["C1"].fire_upon
+      @board.cells["D1"].fire_upon
+      expect(@board.render).to eq("  1 2 3 4 \n" +
+                                  "A . . . . \n" +
+                                  "B . . . . \n" +
+                                  "C X . . . \n" +
+                                  "D X . . . \n")
+      end
+
+     it 'can show ships place, hits and misses and sunk ships altogether' do
+
+        @board.place_ship(@cruiser, ["A1", "A2", "A3"])
+        @board.cells["A1"].fire_upon
+        @board.cells["B4"].fire_upon
+        @board.place_ship(@submarine, ["C1", "D1"])
+        @board.cells["C1"].fire_upon
+        @board.cells["D1"].fire_upon
+        expect(@board.render(true)).to eq("  1 2 3 4 \n" +
+                                          "A H S S . \n" +
+                                          "B . . . M \n" +
+                                          "C X . . . \n" +
+                                          "D X . . . \n")  
+      end
+            
   end
+ 
+end
 
