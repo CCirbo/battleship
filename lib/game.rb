e3cb010713a0530b0c3cb1cda746b2d71215501b
James-Cochran <jameslikesfruit@gmail.com>
2024-05-29 17:30:39 -0400

    tidied up indentation and spacing in all ruby and spec files.  Added to and completed readme file.

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,8 @@ class Game
     end
 
     def display_main_menu
-        puts "Welcome to BATTLESHIP\n"
+        puts "Welcome to BATTLESHIP \n "
+
         puts "Enter 'p' to play or 'q' to quit."
     end
 
@@ -33,7 +34,6 @@ class Game
                 puts "Invalid please enter 'p' to play or 'q' to quit."
                 self.start
             end
-           
     end
    
     def run_game
@@ -42,11 +42,7 @@ class Game
         player_ship_placement_prompt
         player_ship_placement(@player_cruiser, 0)
         player_ship_placement(@player_submarine, 1)
-        # turn_start
-        # player_turn_shot
-        # computer_turn_shot
         play_game
-        #end_game
     end
    
     def computer_ship_placement(ship)
@@ -54,70 +50,55 @@ class Game
         until @computer_board.valid_placement?(ship, random_coords)
             random_coords = @computer_board.cells.keys.sample(ship.length) 
         end
-        @computer_board.place_ship(ship, random_coords)     
+            @computer_board.place_ship(ship, random_coords)     
     end
     
     def player_ship_placement_prompt
-        puts "I have laid out my ships on the grid."
-        puts "You now need to lay out your two ships."
-        puts "The Cruiser is three units long and the Submarine is two units long."
+        puts "I have laid out my ships on the grid. \n "
+        puts "You now need to lay out your two ships. \n "
+        puts "The Cruiser is three units long and the Submarine is two units long. \n "
     end
     
     def player_ship_placement(ship, show)
-       
         show == 0 ? (puts @player_board.render) : (puts @player_board.render(true))
-        puts "Enter the squares for the #{ship.name} (#{ship.length} spaces (ie. A1, A2, A3)):"
-        user_input = gets.chomp.upcase.split(", ")
+            puts " \n Enter the squares for the #{ship.name} (#{ship.length} spaces (ie. A1, A2, A3)): \n "
+            user_input = gets.chomp.upcase.split(", ")
         until @player_board.valid_placement?(ship, user_input)
             puts "Invalid placement. Must be consecutive and not diagonal"
             user_input = gets.chomp.upcase.split(", ")
         end
         @player_board.place_ship(ship, user_input)
-        puts "Your #{ship.name} has been placed."
+        puts " \n Your #{ship.name} has been placed. \n "
     end
 
     def turn_start
-        puts "==========COMPUTER BOARD========="
-         puts @computer_board.render
-        puts "==========PLAYER BOARD==========="
+        puts "==========COMPUTER BOARD========= \n "
+        puts @computer_board.render
+        puts " \n ==========PLAYER BOARD=========== \n "
         puts @player_board.render(true)
     end
     
     def player_turn_shot
-        #binding.pry
-        #prompt the player for a coordinate to fire upon
-        puts "Enter the coordinate for your shot:"
-        #receives user input of the coordinate, removes the return keystroke, and capitalizes
-        user_input = gets.chomp.upcase
-        #validates if the user input is a valid coordinate on the computer board AND if the user input is a computer cell that has not been fired upon.
+        puts " \n Enter the coordinate for your shot: \n "
+            user_input = gets.chomp.upcase
         until @computer_board.valid_coordinate?(user_input) && !@computer_board.cells[user_input].fired_upon?
-            #if either condition is false, asks for a valid coordinate
-            puts "Please enter a valid coordinate:"
+        puts "You have already fired here.  Enter a new coordinate:"
             user_input = gets.chomp.upcase
         end
-        #if both conditions are true it registers a user input shot on the computer board
-        @computer_board.cells[user_input].fire_upon
-        #returns a rendered board showing the players shot
+            @computer_board.cells[user_input].fire_upon
         puts @computer_board.render(true)
-        puts "Your shot on #{user_input} was a #{@computer_board.cells[user_input].render}"
+        puts "Your shot on #{user_input} was a #{@computer_board.cells[user_input].render} \n "
     end
 
     def computer_turn_shot
-       # computer selects a random coordinate using sample of the cell instance hash keys
-        random_coords = @player_board.cells.keys.sample
-        # until loop, are the random coords valid on the player board
-        # AND the player board coordinate is not showing fired upon
+            random_coords = @player_board.cells.keys.sample
         until @player_board.valid_coordinate?(random_coords) && !@player_board.cells[random_coords].fired_upon?
             random_coords = @player_board.cells.keys.sample
         end
-        #will continue until loop until both conditions are met then 
-        #registers that the player board has been hit
-        @player_board.cells[random_coords].fire_upon
-       #returs
+            @player_board.cells[random_coords].fire_upon
         puts @player_board.render(true)
-        puts "My shot on #{random_coords} was a #{@player_board.cells[random_coords].render}"
+        puts "My shot on #{random_coords} was a #{@player_board.cells[random_coords].render} \n "
     end
-
 
     def play_game
         until [@computer_cruiser, @computer_submarine].all?(&:sunk?) || [@player_cruiser, @player_submarine].all?(&:sunk?)
@@ -129,13 +110,12 @@ class Game
         end
         end_game
     end
-
     
-        def end_game
-            if [@player_cruiser, @player_submarine].all?(&:sunk?)
-            puts "I Won!"
-            else
-            puts "You Won!"
+    def end_game
+        if [@player_cruiser, @player_submarine].all?(&:sunk?)
+            puts "I Won! Let's play again! \n "
+        else
+            puts "You Won! Let's play again! \n "
         end
          start
     end
--- spec/readme.md
+++ spec/readme.md
@@ -1,13 +1,15 @@
 Iteration 3 did not provide an interaction pattern.
 
 How did you approach designing this iteration? If you did not get to Iteration 3, reflect on how you think you would’ve approached the design and problem solving process.
-    We started off by reading it over, and figuring out just HOW to start. James did a great job making a new class lib and spec files and started us down the road with the initial code method and testing.
+    For the play of the game, our approach was to take it one step at a time.  Trying to visualize setting up and playing a real physical game.  Essentially, decide to play Battleship.  Open up the boards, place ships, and record shots back and forth until one persons ships have all been hit and sunk.
+    
+   
 If you had one more day to work on this project, what would you work on?
-    Getting through iteration 3, then looking over all the classes and refactoring if needed,
-    then trying iteration 4.
+    Aesthetics for sure.  Being able to spend more time researching different formatting methods and structure for ruby.  It would definetly improve the games terminal playability.  Also, having enough gas in the tank to look at iteration 4.
 
 Describe the pairing techniques you used while working on this project. 
-    We did a lot of group work time, via zoom and homeroom, we talked everyday in slack and wrote comments in the PR requests. We did a lot of driver/navigator working style
+    We did a lot of group work time, via zoom and slack huddles. We talked everyday in slack and wrote comments in the PR requests. We did a lot of driver/navigator working style. We were lucky to have compatible scheduling to work together live for most of the project.  There was still solo work being merged and discussed, but the bulk was in person coopertive pairing.
 
 Describe how feedback was shared over the course of this project. 
     We again talked every day, either with doing the coding together or updating each other when we did something on our own. We did this via slack and comments on the pull requests. We also left commented out messages in the lib or spec files for further clarification of what we had done.
+    
